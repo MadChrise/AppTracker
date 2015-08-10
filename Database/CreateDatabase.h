d431d776799510332d2d7d23ad71051374036e11
MadChrise <MadChrise@users.noreply.github.com>
2015-08-10 18:52:15 +0200

    Added Class for Database creation

---
+++ Database/CreateDatabase.h
@@ -0,0 +1,15 @@
+#ifndef CreateDatabaseH
+#define CreateDatabaseH
+//---------------------------------------------------------------------------
+
+class CreateDatabase
+{
+	private:
+	public:
+		// Constructor, Copyconstructor, Destructor
+		CreateDatabase();
+		CreateDatabase(const CreateDatabase &createDatabase);
+		~CreateDatabase();
+};
+//---------------------------------------------------------------------------
+#endif
