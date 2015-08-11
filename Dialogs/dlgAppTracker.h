e7f224e5ede9171d83854a0ee3d575883738ec23
MadChrise <MadChrise.@users.noreply.github.com>
2015-08-11 06:49:36 +0200

    Added Headerfiles

--- Dialogs/dlgAppTracker.h
+++ Dialogs/dlgAppTracker.h
@@ -5,6 +5,19 @@
 #include <System.Classes.hpp>
 #include <FMX.Controls.hpp>
 #include <FMX.Forms.hpp>
+#include <Data.DB.hpp>
+#include <FireDAC.Comp.Client.hpp>
+#include <FireDAC.Phys.hpp>
+#include <FireDAC.Phys.Intf.hpp>
+#include <FireDAC.Stan.Async.hpp>
+#include <FireDAC.Stan.Def.hpp>
+#include <FireDAC.Stan.Error.hpp>
+#include <FireDAC.Stan.Intf.hpp>
+#include <FireDAC.Stan.Option.hpp>
+#include <FireDAC.Stan.Pool.hpp>
+#include <FireDAC.UI.Intf.hpp>
+#include <FireDAC.Comp.UI.hpp>
+#include <FireDAC.FMXUI.Wait.hpp>
 //---------------------------------------------------------------------------
 
 class TFormAppTracker : public TForm
