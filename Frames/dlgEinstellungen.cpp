0702275326fe32781408e89348883df42fcce46f
MadChrise <MadChrise@users.noreply.github.com>
2015-09-28 12:46:50 +0200

    Added a new Preferences-Dialog

---
+++ Frames/dlgEinstellungen.cpp
@@ -0,0 +1,18 @@
+#include <fmx.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+
+#include "dlgEinstellungen.h"
+//---------------------------------------------------------------------------
+
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+
+TFormEinstellungen *FormEinstellungen;
+//---------------------------------------------------------------------------
+
+__fastcall TFormEinstellungen::TFormEinstellungen(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
---
+++ Frames/dlgEinstellungen.h
@@ -0,0 +1,20 @@
+#ifndef dlgEinstellungenH
+#define dlgEinstellungenH
+//---------------------------------------------------------------------------
+
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+//---------------------------------------------------------------------------
+
+class TFormEinstellungen : public TForm
+{
+__published:	// Von der IDE verwaltete Komponenten
+private:	// Benutzer-Deklarationen
+public:		// Benutzer-Deklarationen
+	__fastcall TFormEinstellungen(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFormEinstellungen *FormEinstellungen;
+//---------------------------------------------------------------------------
+#endif
